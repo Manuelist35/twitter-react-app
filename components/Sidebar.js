80e39686754c1f39d4c5c19190af34c7d784a4f4
Manuelist35 <manuelistenango@gmail.com>
2022-05-02 13:39:15 -0700

    added some changes in the sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Image from "next/image";
+import SidebarLink from"../components/SidebarLink";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="hidden sm:flex flex-col items-center xl:items-start
+    xl:w-[340px] p-2 fixed h-full">
+      <div className="hoverAnimation flex items-center justify-center w-14 h-14 p-0 xl:ml-24">
+        <Image src="https://rb.gy/ogau5a" width={30} height={30}/>
+      </div>
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
